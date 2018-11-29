f3a2ef3bc97ceb4c5f4b822fa9c1d9d052930879
Nigel Liang <nigel@nigelliang.com>
2018-11-28 18:00:14 -0800

    Read password from stdin and not cmdline flag

--- unlock_my_tesla.py
+++ unlock_my_tesla.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
+import getpass
 import time
 
 import myTesla
 
 parser = argparse.ArgumentParser(description='Unlock my tesla.')
 parser.add_argument('--username', help='Tesla username', required=True)
-parser.add_argument('--password', help='Tesla password', required=True)
 parser.add_argument('--interval', help='Polling interval in seconds', default=300)
 
 args = parser.parse_args()
 
+password = getpass.getpass('Tesla password: ')
+
 while True:
-    conn = myTesla.connect(args.username, args.password)
+    conn = myTesla.connect(args.username, password)
     vehicle_state = conn.vehicle_state()
 
     if 'error' in vehicle_state:
